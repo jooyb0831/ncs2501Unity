1a90e36f5ef27e2a220b6ce8c1eb392e30a9a6d2
jooyb0831 <dia231@naver.com>
2025-03-06 15:48:39 +0900

    0306

--- Assets/Scripts/Old/DelegateDemo.cs
+++ Assets/Scripts/Old/DelegateDemo.cs
@@ -9,6 +9,7 @@ public class DelegateDemo : MonoBehaviour
 
     //델리게이트 선언
     delegate float SumHandler(float a, float b);
+
     //델리게이트 타입의 변수 선언
     SumHandler sumHandler;
 
@@ -26,6 +27,16 @@ public class DelegateDemo : MonoBehaviour
         float sum = sumHandler(10.0f, 0.5f);
         //결괏값 출력
         Debug.Log($"Sum = {sum}");
+
+        //델리게이트 변수에 람다식 선언
+        sumHandler = (float a, float b) => (a+b);
+        float sum2 = sumHandler(10.0f, 5.0f);
+        Debug.Log($"Sum2 : {sum2}");
+
+        //델리게이트 변수에 무명 메서드 연결
+        sumHandler = delegate(float a, float b){return a+b;};
+        float sum3 = sumHandler(2.0f, 3.0f);
+        Debug.Log($"Sum3 : {sum3}");
     }
 
     // Update is called once per frame
